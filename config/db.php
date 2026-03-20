99463a117e753706c51f9f02d5311ebebce3ff29
Qu1styy <ameserakov304@gmail.com>
2026-03-20 09:08:31 +0700

    Working with the database

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'pkpkrges_m4',
+    'username' => 'pkpkrges',
+    'password' => 'idfKf9',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
--- core/Src/Settings.php
+++ core/Src/Settings.php
@@ -29,5 +29,11 @@ class Settings
     public function getViewsPath(): string
     {
         return '/' . $this->path['views'] ?? '';
+
+    }
+
+    public function getDbSetting(): array
+    {
+        return $this->db ?? [];
     }
 }
